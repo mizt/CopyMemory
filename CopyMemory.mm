fedf4e714ee47368c95a4815c091d5a9bb053c78
mizt <info@mizt.org>
2020-12-12 12:08:27 +0900

    Initial Commit

---
+++ CopyMemory.mm
@@ -0,0 +1,60 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[]) {
+	@autoreleasepool {
+		
+		int w = 1920;
+		int h = 1080;
+		
+		srandom(CFAbsoluteTimeGetCurrent());
+		
+		uint32 *src = new uint32[w*h];
+		for(int k=0; k<w*h; k++) {
+			src[k] = 0xFF000000|random()%0xFFFFFF;
+		}
+
+		uint32 *dst = new uint32[w*h];
+		
+		double sum = 0;
+		
+		for(int n=0; n<100; n++) {
+			double then = CFAbsoluteTimeGetCurrent();
+			for(int i=0; i<h; i++) {
+				uint32 *p = src+i*w;
+				uint32 *q = dst+i*w;
+				for(int j=0; j<w; j++) {
+					*p++ = *q++;
+				}
+			}
+			double diff = CFAbsoluteTimeGetCurrent()-then;
+			NSLog(@"%f",diff);
+			sum+=diff;
+		}
+		
+		NSLog(@"rows = %f",sum/100.0);
+		
+		sum = 0;
+		
+		for(int n=0; n<100; n++) {
+			double then = CFAbsoluteTimeGetCurrent();
+			for(int j=0; j<w; j++) {
+				uint32 *p = src+j;
+				uint32 *q = dst+j;
+				for(int i=0; i<h; i++) {
+					*p = *q;
+					p+=w;
+					q+=w;
+				}
+			}
+			double diff = CFAbsoluteTimeGetCurrent()-then;
+			NSLog(@"%f",diff);
+			sum+=diff;
+		}
+		
+		NSLog(@"cols = %f",sum/100.0);
+		
+		delete[] dst;
+		delete[] src;
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Copy
+
+	MacBook Air (M1, 2020)
+	Chip: M1
+	Memory: 16GB
+
+vs.
+
+	MacBook Air (Retina, 13-inch, 2019)
+	Processor: 1.6GHz Dual Intel Core i5
+	Memory: 8GB 2133 MHz LPDDR3
+	Graphics: Intel UHD Graphics 617 1536MB
+
+
+### Build
+
+`clang++ -std=c++17 -Wc++17-extensions -fobjc-arc -O3 -framework Cocoa ./CopyMemory.mm -o ./CopyMemory`
+
+### Benchmark
+
+Benchmark of memory copy speed in row and column directions.
+
+||rows|cols|
+|:--|:-:|:-:|
+|M1|0.00001|0.0005|
+|Intel|0.002|0.045|
